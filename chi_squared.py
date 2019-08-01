91c2e1e25af620b9efe86821c243a6bb6aec5853
nehagaur-code <gaurneha32@gmail.com>
2019-08-01 19:35:54 +0530

    calculate importance

---
+++ chi_squared.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 13:22:44 2019
+
+@author: neha
+"""
+
+# Chi squared test
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+from scipy.stats import chi2_contingency
+
+class ChiSquare:
+    def __init__(self, dataframe):
+        self.df = dataframe
+        self.p = None #P-Value
+        self.chi2 = None #Chi Test Statistic
+        self.dof = None
+        
+        self.dfObserved = None
+        self.dfExpected = None
+        
+    def _print_chisquare_result(self, colX, alpha):
+        result = ""
+        if self.p<alpha:
+            result="{0} is IMPORTANT for Prediction".format(colX)
+        else:
+            result="{0} is NOT an important predictor. (Discard {0} from model)".format(colX)
+
+        print(result)
+        
+    def TestIndependence(self,colX,colY, alpha=0.05):
+        X = self.df[colX].astype(str)
+        Y = self.df[colY].astype(str)
+        
+        self.dfObserved = pd.crosstab(Y,X) 
+        chi2, p, dof, expected = stats.chi2_contingency(self.dfObserved.values)
+        self.p = p
+        self.chi2 = chi2
+        self.dof = dof 
+        
+        self.dfExpected = pd.DataFrame(expected, columns=self.dfObserved.columns, index = self.dfObserved.index)
+        
+        self._print_chisquare_result(colX,alpha)
+
+df = pd.pandas.read_csv("titanic.csv")
+
+#Initialize ChiSquare Class
+cT = ChiSquare(df)
+
+#Feature Selection
+testColumns = list(df.columns.values)
+for var in testColumns:
+    cT.TestIndependence(colX=var,colY="Survived" )  
